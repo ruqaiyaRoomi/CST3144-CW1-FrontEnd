216cff22a3cdfc286820d1ac2aa8815503ff6dca
RuqaiyaRoomi <ruqaiyaroomi.com>
2025-11-23 10:51:40 +0400

    formatted code

--- script.js
+++ script.js
@@ -1,242 +1,233 @@
-addToCartButton = document.getElementsByClassName("addButton");
+let afterSchool = new Vue({
+  el: "#app",
+  data: {
+    lessons: [], // Array for lessons
+    cart: [], // Cart
+    key: "", // sorting key
+    order: "", // sort
+    showCheckout: true,
+    showbill: false,
+    orderInformation: {
+      nameInput: "",
+      phoneNumber: "",
+      email: "",
+      subject: [],
+      spaces: [],
+      lessonId: [],
+    },
+    searchInput: "",
+  },
 
-            let afterSchool = new Vue ({
-                el: "#app",
-                data:{
-                        lessons: [],
-                            cart: [],
-                            checkout: [],
-                            key: '',
-                            order: '',
-                            showCheckout: true,
-                            showbill: false,
-                        orderInformation: {
-                                nameInput: '',
-                                phoneNumber: '',
-                                email: '',
-                                subject: [],
-                                spaces:[],
-                                lessonId: [] 
-                             },
-                        searchInput: "",
+  created: function () {
+    // Fetch all lessons from backend
+    fetch("https://cst3145-cw1-backend.onrender.com/Afterschool/lesson").then(
+      function (response) {
+        response.json().then(function (json) {
+          afterSchool.lessons = json.map((lesson) => ({
+            ...lesson,
+            initalSpaces: lesson.spaces,
+          }));
+        });
+      }
+    );
+  },
 
-                            
-                    },
+  methods: {
+    // Add lesson to cart if space is available
+    addToCart: function (lesson) {
+      if (lesson.spaces > 0) {
+        this.cart.push(lesson);
+        lesson.spaces--;
+      }
+    },
+    // remove lesson from cart and restore space
+    removeFromCart: function (lesson) {
+      const index = this.cart.indexOf(lesson);
+      if (index !== -1) {
+        this.cart.splice(index, 1);
+        lesson.spaces++;
+      }
+    },
 
-                     created: function () {
-                        fetch("https://cst3145-cw1-backend.onrender.com/Afterschool/lesson"). then(
-                        function(response) {
-                            response.json().then (
-                                function(json){
-                                    afterSchool.lessons = json.map(lesson => ({
-                                        ...lesson,
-                                        initalSpaces: lesson.spaces
-                                    })); 
-                                }
-                            )
-                        }
-                    )
+    // checks if lesson is full
+    isFull: function (lesson) {
+      return lesson.spaces === 0;
+    },
+
+    // Toggle checkout
+    showCart: function () {
+      if (!this.showCheckout && this.cart.length === 0) return;
+
+      this.showCheckout = !this.showCheckout;
+    },
+    // go back to main page
+    goBack: function () {
+      this.showCheckout = true;
+    },
+
+    // saves order info
+    saveOrderInfo: function () {
+      const lessonIds = this.cart.map((lesson) => lesson._id);
+      const subject = this.cart.map((lesson) => lesson.subject);
+      const spaces = [];
+
+      // Count number of times each lesson appears in cart
+      lessonIds.forEach((id, i) => {
+        if (lessonIds.indexOf(id) === i) {
+          let count = 0;
+          lessonIds.forEach((countId) => {
+            if (countId === id) count++;
+          });
+          spaces.push(count);
+        }
+      });
+      // POST order to backend
+      fetch("https://cst3145-cw1-backend.onrender.com/Afterschool/orderInfo ", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: this.orderInformation.nameInput,
+          phoneNumber: this.orderInformation.phoneNumber,
+          email: this.orderInformation.email,
+          subject: subject,
+          spaces: spaces,
+          lessonId: lessonIds,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Order saved:", data);
+          this.showbill = true;
+
+          // Update lesson spaces in backend after successful order
+          for (let i = 0; i < this.cart.length; i++) {
+            const lesson = this.cart[i];
+            const booked =
+              spaces[this.cart.map((l) => l._id).indexOf(lesson._id)];
+
+            // calculates the updated spaces
+            const updatedSpaces = lesson.initalSpaces - booked;
+            console.log("Updated space:");
+            console.log(updatedSpaces);
+            // Update backend for each lesson
+            fetch(
+              `https://cst3145-cw1-backend.onrender.com/Afterschool/lesson/${lesson._id}`,
+              {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
                 },
+                body: JSON.stringify({
+                  spaces: updatedSpaces,
+                }),
+              }
+            )
+              .then((response) => response.json())
+              .then((update) => {
+                console.log(`Lesson ${lesson} updated to: `, updatedSpaces);
+              });
+          }
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
 
-                methods: {
-                    addToCart: function(lesson) {
-                        if(lesson.spaces > 0){
-                        this.cart.push(lesson);
-                        lesson.spaces--;
-                        }
-                        
-                    },
+    // Search lessons based on keyword
+    search: function () {
+      const keyword = this.searchInput;
+      fetch(
+        `https://cst3145-cw1-backend.onrender.com/Afterschool/lesson/search?q=${keyword}`
+      ).then(function (response) {
+        response.json().then(function (json) {
+          afterSchool.lessons = json;
+        });
+      });
+    },
 
-                    removeFromCart: function(lesson){
-                        const index =  this.cart.indexOf(lesson);
-                        if(index !== -1){
-                        this.cart.splice(index, 1)
-                        lesson.spaces++;
-                        }
-                    },
+    // Close bill and reload page
+    closeBill: function () {
+      this.showbill = false;
+      window.location.reload();
+    },
+  },
 
-                    isFull: function(lesson){
-                    return lesson.spaces === 0;
-                    },
-                    
+  computed: {
+    // Count items in cart
+    cartCount: function () {
+      return this.cart.length;
+    },
 
-                     showCart: function() {
-                    
-                    if(!this.showCheckout && this.cart.length === 0)
-                        return;
-                    
-                    
-                     this.showCheckout = !this.showCheckout; 
-        
-                },
+    // sorting function based on key and order
+    sortfunction: function () {
+      let sortedList = this.lessons.slice();
 
-                goBack: function() {
-                        this.showCheckout = true;
-                    },
+      if (!this.key || !this.order) {
+        return sortedList;
+      }
+      const order = this.order === "asc" ? 1 : -1;
 
-                saveOrderInfo: function() {
+      if (this.key === "subject") {
+        return sortedList.sort(
+          (a, b) => a.subject.localeCompare(b.subject) * order
+        );
+      } else if (this.key === "price") {
+        return sortedList.sort((a, b) => (a.price - b.price) * order);
+      } else if (this.key === "location") {
+        return sortedList.sort(
+          (a, b) => a.location.localeCompare(b.location) * order
+        );
+      } else if (this.key === "spaces") {
+        return sortedList.sort((a, b) => (a.spaces - b.spaces) * order);
+      } else {
+        return sortedList;
+      }
+    },
 
-                        const lessonIds = this.cart.map(lesson => lesson._id)
-                        const subject = this.cart.map(lesson => lesson.subject)
-                        const spaces = [];
-                        lessonIds.forEach((id,i)  => {
+    // Validation for checkout fields
 
-                            if(lessonIds.indexOf(id) === i) {
-                                 let count = 0;
-                            lessonIds.forEach(countId =>{
-                                if(countId === id) count++;
-                            });
-                            spaces.push(count)
-                     
-                            }
-                        });
+    validatePhone: function () {
+      let regex = /^\d{10}$/;
+      let phoneNumber = this.orderInformation.phoneNumber.trim();
+      return !!(regex.test(phoneNumber) && phoneNumber);
+    },
 
-                    fetch("https://cst3145-cw1-backend.onrender.com/Afterschool/orderInfo ", {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
-                        },
-                        body: JSON.stringify({
-                            name: this.orderInformation.nameInput,
-                            phoneNumber: this.orderInformation.phoneNumber,
-                            email: this.orderInformation.email,
-                            subject: subject,
-                            spaces: spaces,
-                            lessonId: lessonIds,
-                            
-                        })
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log("Order saved:", data);
-                        this.showbill = true;
-                        
-                        for(let i = 0; i< this.cart.length; i++) {
-                            const lesson = this.cart[i];
-                            const booked = spaces[this.cart.map(l => l._id).indexOf(lesson._id)];
-                        
+    validateName: function () {
+      let regex = /^[A-Za-z]+$/;
+      let name = this.orderInformation.nameInput.trim();
+      return !!(regex.test(name) && name);
+    },
 
-                            const updatedSpaces = lesson.initalSpaces - booked ;
-                            console.log("Updated space:")
-                            console.log(updatedSpaces)
+    validateEmail: function () {
+      let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      let email = this.orderInformation.email.trim();
 
-                            fetch(`https://cst3145-cw1-backend.onrender.com/Afterschool/lesson/${lesson._id}`, {
-                                method: 'PUT',
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify({
-                                    spaces: updatedSpaces
-                                    
-                                })
-                            }).then(response =>response.json())
-                                .then(update => {
-                                    console.log(`Lesson ${lesson} updated to: `, updatedSpaces)
-                                })
-                        }
-                    })
-                    .catch(error => {
-                        console.error("Error:", error);
-                    });
+      return !!(regex.test(email) && email);
+    },
 
-                   
-                },
+    // checks if all validations are successful
+    isFormFilled: function () {
+      console.log(
+        this.validateEmail && this.validateName && this.validatePhone
+      );
+      return this.validateEmail && this.validateName && this.validatePhone;
+    },
 
+    // shows validation errors
 
-                search: function () {
-                        const keyword = this.searchInput
-                        fetch(`https://cst3145-cw1-backend.onrender.com/Afterschool/lesson/search?q=${keyword}`). then(
-                        function(response) {
-                            response.json().then (
-                                function(json){
-                                    afterSchool.lessons = json
-                                }
-                            )
-                        }
-                    )
-                },
-    
-                
-                closeBill: function(){
-                    this.showbill = false;
-                    window.location.reload();
-              
-                },
+    showPhoneError: function () {
+      return (
+        this.orderInformation.phoneNumber.length > 0 && !this.validatePhone
+      );
+    },
 
-            
-                   
-                },
-
-
-                computed: {
-                    cartCount: function() {
-                        return this.cart.length;
-                    },
-                
-
-                    sortfunction:  function(){
-                            let  sortedList =  this.lessons.slice();
-
-                            if(!this.key || !this.order){
-                                return sortedList
-                            }
-                            const order = this.order === 'asc' ? 1 : -1;
-
-                            if(this.key === 'subject') {
-                                return sortedList.sort((a,b) => a.subject.localeCompare(b.subject) * order)
-                        
-                            } else if (this.key === 'price') {
-                               return sortedList.sort((a,b) => (a.price - b.price) * order
-                                )
-                            } else if (this.key === 'location') {
-                               return sortedList.sort((a,b) => a.location.localeCompare(b.location) * order
-                                )
-                            } else if (this.key === 'spaces') {
-                                return sortedList.sort((a,b) =>
-                                    (a.spaces - b.spaces) * order
-                                ) }
-                            else  {
-                               return sortedList }
-                    },
-
-
-                   validatePhone: function() {
-                     let regex = /^\d{10}$/;
-                     let phoneNumber = this.orderInformation.phoneNumber.trim() ;
-                     return !!(regex.test(phoneNumber) && phoneNumber)
-                   },
-
-                   validateName: function(){
-                    let regex = (/^[A-Za-z]+$/);
-                    let name = this.orderInformation.nameInput.trim()
-                    return !!(regex.test(name) && name)
-                   },
-
-                   validateEmail:function (){
-                    let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-                    let email = this.orderInformation.email.trim();
-
-                    return !!(regex.test(email) && email);
-                   },
-
-                    isFormFilled: function(){
-                        console.log(this.validateEmail && this.validateName && this.validatePhone)
-                    return this.validateEmail && this.validateName && this.validatePhone
-                        
-                   },
-
-                   showPhoneError: function(){
-                    return this.orderInformation.phoneNumber.length > 0 && !this.validatePhone},
-
-                   showNameError: function(){
-                    return this.orderInformation.nameInput.length > 0 && !this.validateName
-                   },
-                   showEmailError: function(){
-
-                    return  this.orderInformation.email.length > 0  && !this.validateEmail;
-
-
-                },},
-
-    
-            })
+    showNameError: function () {
+      return this.orderInformation.nameInput.length > 0 && !this.validateName;
+    },
+    showEmailError: function () {
+      return this.orderInformation.email.length > 0 && !this.validateEmail;
+    },
+  },
+});
